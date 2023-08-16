8ef339f872736f161b7eba9156f2dc67870e8266
Sa1tyPretzel <mithileshpawar1@gmail.com>
2023-08-16 17:10:34 +0530

    firebase setup

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// Options for Player Colors... these are in the same order as our sprite sheet
+const playerColors = ["blue", "red", "orange", "yellow", "green", "purple"];
+
 //Misc Heplers
 function randomFromArray(array) {
     return array[Math.floor(Math.random() * array.length)];
@@ -6,9 +9,85 @@ function getKeyString(x, y) {
     return `${x}x${y}`;
 }
 
+function createName() {
+    const prefix = randomFromArray([
+      "COOL",
+      "SUPER",
+      "HIP",
+      "SMUG",
+      "COOL",
+      "SILKY",
+      "GOOD",
+      "SAFE",
+      "DEAR",
+      "DAMP",
+      "WARM",
+      "RICH",
+      "LONG",
+      "DARK",
+      "SOFT",
+      "BUFF",
+      "DOPE",
+    ]);
+    const animal = randomFromArray([
+      "BEAR",
+      "DOG",
+      "CAT",
+      "FOX",
+      "LAMB",
+      "LION",
+      "BOAR",
+      "GOAT",
+      "VOLE",
+      "SEAL",
+      "PUMA",
+      "MULE",
+      "BULL",
+      "BIRD",
+      "BUG",
+    ]);
+    return `${prefix} ${animal}`;
+  }
+
 (function () {
 
-    firebase.auth().signInAnonymously()
+    let playerId;
+    let playerRef;
+
+    firebase.auth().onAuthStateChanged((user) => {
+        console.log(user)
+        if (user) {
+            //logged in
+            playerId = user.uid;
+            playerRef = firebase.database().ref(`players/${playerId}`);
+
+            const name = createName();
+
+            playerRef.set({
+                id: playerId,
+                name,
+                direction: "right",
+                color: randomFromArray(playerColors),
+                x: "3",
+                y: "3",
+                coins: 0
+            })
+
+            //Remove from Firebase when disconnect
+            playerRef.onDisconnect().remove();
+
+            
+        } else {
+            //logged out
+        }
+    })
+
+    firebase.auth().signInAnonymously().catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+        console.log(errorCode, errorMessage);
+    });
 
 
 }) ();
